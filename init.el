3f4759fea7c7019c3566b155327354a76ad51044
Jakob Holderbaum <jakob@featurefabrik.de>
2013-11-06 21:07:51 +0100

    Load all .el files from init.d directory

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 ;; Customizations shall be stored in custom.el
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(setq custom-file 
+      (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
+
 
 ;; Marmalade package repo
 (require 'package)
@@ -8,3 +10,11 @@
     '("marmalade" .
       "http://marmalade-repo.org/packages/"))
 (package-initialize)
+
+(setq init-dir 
+      (expand-file-name "init.d" user-emacs-directory))
+
+;; Load all elisp files in ./init.d
+(if (file-exists-p init-dir)
+    (dolist (file (directory-files init-dir t "\\.el$"))
+      (load file)))
