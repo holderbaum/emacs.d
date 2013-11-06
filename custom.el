adcbe95da8382b631e1a2c26c3db09c9d3a81171
Jakob Holderbaum <jakob@featurefabrik.de>
2013-11-06 20:30:00 +0100

    Add custom ido settings

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ido-enable-flex-matching t)
+ '(ido-mode (quote both) nil (ido))
  '(scroll-bar-mode nil)
  '(size-indication-mode t)
  '(tool-bar-mode nil))
