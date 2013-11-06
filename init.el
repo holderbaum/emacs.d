3c819bc1f6c41a2f21e69a16ac54ac676d3ed5f2
Jakob Holderbaum <jakob@featurefabrik.de>
2013-11-06 19:57:49 +0100

    Add marmalage repo to package archives

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
 ;; Customizations shall be stored in custom.el
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
+
+;; Marmalade package repo
+(require 'package)
+(add-to-list 'package-archives 
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+(package-initialize)
