28fb7aa710d313297bc0828ef06fa6173ba68c79
Jakob Holderbaum <jakob@featurefabrik.de>
2013-11-06 21:43:22 +0100

    Remove package repo from init

--- init.el
+++ init.el
@@ -3,14 +3,6 @@
       (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
 
-
-;; Marmalade package repo
-(require 'package)
-(add-to-list 'package-archives 
-    '("marmalade" .
-      "http://marmalade-repo.org/packages/"))
-(package-initialize)
-
 (setq init-dir 
       (expand-file-name "init.d" user-emacs-directory))
 
