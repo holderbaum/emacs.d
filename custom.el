cbf621f0030d96b2f7ab5e1226d4a7af4d727368
Jakob Holderbaum <code@jakob.io>
2014-03-23 20:06:19 +0100

    Configure darker whitespace indicators

--- custom.el
+++ custom.el
@@ -15,4 +15,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(whitespace-newline ((t (:foreground "dark gray" :weight normal))))
+ '(whitespace-space ((t (:background "LightYellow" :foreground "dark gray"))))
+ '(whitespace-tab ((t (:background "beige" :foreground "dark gray")))))
