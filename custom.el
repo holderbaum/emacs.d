347b1dec1aee88dd1d887cba546d28eb9f95bd60
Jakob Holderbaum <code@jakob.io>
2014-03-24 05:05:34 +0100

    Add styling for markdown headers

--- custom.el
+++ custom.el
@@ -17,4 +17,12 @@
  ;; If there is more than one, they won't work right.
  '(whitespace-newline ((t (:foreground "dark gray" :weight normal))))
  '(whitespace-space ((t (:background "LightYellow" :foreground "dark gray"))))
- '(whitespace-tab ((t (:background "beige" :foreground "dark gray")))))
+ '(whitespace-tab ((t (:background "beige" :foreground "dark gray"))))
+ '(markdown-header-delimiter-face ((t (:inherit font-lock-function-name-face :underline t :weight bold))) t)
+ '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.5))) t)
+ '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.3))) t)
+ '(markdown-header-face-3 ((t (:inherit markdown-header-face :underline t :height 1.2))) t)
+ '(markdown-header-face-4 ((t (:inherit markdown-header-face :underline t :height 1.1))) t)
+ '(markdown-header-face-5 ((t (:inherit markdown-header-face :underline t))) t)
+ '(markdown-header-face-6 ((t (:inherit markdown-header-face :underline t))) t))
+
