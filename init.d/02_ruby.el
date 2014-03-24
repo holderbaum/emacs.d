afba1e2b12d2b43474d92120ad14c1dc438a946c
Jakob Holderbaum <code@jakob.io>
2014-03-24 17:11:30 +0100

    Add error definition for ruby, WIP

--- init.d/02_ruby.el
+++ init.d/02_ruby.el
@@ -5,6 +5,8 @@
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
+
+(require 'compile)
  
 (add-hook 'enh-ruby-mode-hook 'rubocop-mode)
 (add-hook 'enh-ruby-mode-hook 'whitespace-mode)
@@ -14,6 +16,10 @@
 	  (lambda () (rvm-activate-corresponding-ruby)))
 
 
+(add-hook 'enh-ruby-mode-hook
+	  (lambda ()
+	    (add-to-list 'compilation-error-regexp-alist
+			 '("\[\\(.*?\\):\\([0-9]+\\)\]:$" 1 2))))
 (require 'smartparens-ruby)
 
 (sp-with-modes '(rhtml-mode)
