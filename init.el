e17ed98c72c16e98fc9c96a061e24e6d41841565
Jakob Holderbaum <code@jakob.io>
2014-04-04 12:20:14 +0200

    Add all dirs in vendor/ automatically to load-path

--- init.el
+++ init.el
@@ -6,7 +6,15 @@
 (setq init-dir 
       (expand-file-name "init.d" user-emacs-directory))
 
+(setq vendor-dir
+      (expand-file-name "vendor" user-emacs-directory))
+
 ;; Load all elisp files in ./init.d
 (if (file-exists-p init-dir)
     (dolist (file (directory-files init-dir t "\\.el$"))
       (load file)))
+
+;; Add all vendor packages to load-path
+(if (file-exists-p vendor-dir)
+    (dolist (vendor (directory-files vendor-dir t))
+      (add-to-list 'load-path vendor)))
