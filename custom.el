b8c3404a2aacb5a7e09cf455a0051bbd252abad5
Jakob Holderbaum <code@jakob.io>
2014-04-11 16:37:10 +0200

    Increase emacs fonts

--- custom.el
+++ custom.el
@@ -15,14 +15,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(whitespace-newline ((t (:foreground "dark gray" :weight normal))))
- '(whitespace-space ((t (:background "LightYellow" :foreground "dark gray"))))
- '(whitespace-tab ((t (:background "beige" :foreground "dark gray"))))
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 203 :width normal))))
  '(markdown-header-delimiter-face ((t (:inherit font-lock-function-name-face :underline t :weight bold))) t)
  '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.5))) t)
  '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.3))) t)
  '(markdown-header-face-3 ((t (:inherit markdown-header-face :underline t :height 1.2))) t)
  '(markdown-header-face-4 ((t (:inherit markdown-header-face :underline t :height 1.1))) t)
  '(markdown-header-face-5 ((t (:inherit markdown-header-face :underline t))) t)
- '(markdown-header-face-6 ((t (:inherit markdown-header-face :underline t))) t))
+ '(markdown-header-face-6 ((t (:inherit markdown-header-face :underline t))) t)
+ '(whitespace-newline ((t (:foreground "dark gray" :weight normal))))
+ '(whitespace-space ((t (:background "LightYellow" :foreground "dark gray"))))
+ '(whitespace-tab ((t (:background "beige" :foreground "dark gray")))))
 
