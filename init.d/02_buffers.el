af54ce14482b160036b934c0e7116cfbeff03042
Jakob Holderbaum <code@jakob.io>
2014-04-25 06:17:32 +0200

    Add helpers for closing panes

---
+++ init.d/02_buffers.el
@@ -0,0 +1,17 @@
+(defun close-and-kill-this-pane ()
+  "If there are multiple windows, then close this pane and kill the buffer in it also."
+  (interactive)
+  (kill-this-buffer)
+  (if (not (one-window-p))
+      (delete-window)))
+
+(defun close-and-kill-next-pane ()
+  "If there are multiple windows, then close the other pane and kill the buffer in it also."
+  (interactive)
+  (other-window 1)
+  (kill-this-buffer)
+  (if (not (one-window-p))
+      (delete-window)))
+
+(global-set-key [(control c) (p)] 'close-and-kill-next-pane)
+(global-set-key [(control c) (P)] 'close-and-kill-this-pane)
