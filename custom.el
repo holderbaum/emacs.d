186ba7ddcad964eca4f5945be316799d7e8aa0a0
Jakob Holderbaum <code@jakob.io>
2014-07-30 08:51:21 +0200

    Increase default font size

--- custom.el
+++ custom.el
@@ -16,7 +16,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 98 :width normal))))
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 110 :width normal))))
  '(markdown-header-delimiter-face ((t (:inherit font-lock-function-name-face :underline t :weight bold))))
  '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.5))))
  '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.3))))
