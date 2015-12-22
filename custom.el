6bd9bc9e66168e5f8c5e248869e6b81548791ed2
Jakob Holderbaum <code@jakob.io>
2015-12-22 08:00:00 +0100

    Customize whitespace-related faces less intrusive

--- custom.el
+++ custom.el
@@ -14,3 +14,12 @@
 	     (org-babel-tangle))
 	   nil t)
      (eval load-file "config.el")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-indent ((t (:foreground "dark slate gray"))) t)
+ '(whitespace-hspace ((t (:foreground "dark slate gray"))))
+ '(whitespace-newline ((t (:foreground "dark slate gray"))))
+ '(whitespace-space ((t (:foreground "dark slate gray")))))
